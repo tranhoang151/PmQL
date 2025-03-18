d9d23f81515514210a66c41433d7cab232076d44
tranhoang151 <97266970+tranhoang151@users.noreply.github.com>
2025-03-18 22:53:05 +0700

    Add project files.

---
+++ PmQL/Data/PmQlContext.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using PmQL.Models;
+
+namespace PmQL.Data;
+
+public partial class PmQlContext : DbContext
+{
+    public PmQlContext()
+    {
+    }
+
+    public PmQlContext(DbContextOptions<PmQlContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<FmpA0> FmpA0s { get; set; }
+
+    public virtual DbSet<FmpEvn> FmpEvns { get; set; }
+
+    public virtual DbSet<FmpSai> FmpSais { get; set; }
+
+    public virtual DbSet<NhậpGiáNm> NhậpGiáNms { get; set; }
+
+    public virtual DbSet<PmA0> PmA0s { get; set; }
+
+    public virtual DbSet<PmEvn> PmEvns { get; set; }
+
+    public virtual DbSet<PmSai> PmSais { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Data Source=LAPTOP-409OOSHC\\SQLEXPRESS;Initial Catalog=PmQL;Integrated Security=True;Encrypt=False;Trust Server Certificate=True;Connect Timeout=60");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<FmpEvn>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__FMP_EVN__3214EC276D4638BE");
+        });
+
+        modelBuilder.Entity<FmpSai>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__FMP_Sai__3214EC2740C9C5B8");
+        });
+
+        modelBuilder.Entity<PmEvn>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Pm_EVN__3214EC276B0DD338");
+        });
+
+        modelBuilder.Entity<PmSai>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Pm_Sai__3214EC27797439AB");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ PmQL/Models/PmA0.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace PmQL.Models;
+
+[Keyless]
+[Table("Pm_A0$")]
+public partial class PmA0
+{
+    public double? Ngày { get; set; }
+
+    [StringLength(255)]
+    public string? Giá { get; set; }
+
+    [Column("0h30")]
+    public double? _0h30 { get; set; }
+
+    [Column("1h")]
+    public double? _1h { get; set; }
+
+    [Column("1h30")]
+    public double? _1h30 { get; set; }
+
+    [Column("2h")]
+    public double? _2h { get; set; }
+
+    [Column("2h30")]
+    public double? _2h30 { get; set; }
+
+    [Column("3h")]
+    public double? _3h { get; set; }
+
+    [Column("3h30")]
+    public double? _3h30 { get; set; }
+
+    [Column("4h")]
+    public double? _4h { get; set; }
+
+    [Column("4h30")]
+    public double? _4h30 { get; set; }
+
+    [Column("5h")]
+    public double? _5h { get; set; }
+
+    [Column("5h30")]
+    public double? _5h30 { get; set; }
+
+    [Column("6h")]
+    public double? _6h { get; set; }
+
+    [Column("6h30")]
+    public double? _6h30 { get; set; }
+
+    [Column("7h")]
+    public double? _7h { get; set; }
+
+    [Column("7h30")]
+    public double? _7h30 { get; set; }
+
+    [Column("8h")]
+    public double? _8h { get; set; }
+
+    [Column("8h30")]
+    public double? _8h30 { get; set; }
+
+    [Column("9h")]
+    public double? _9h { get; set; }
+
+    [Column("9h30")]
+    public double? _9h30 { get; set; }
+
+    [Column("10h")]
+    public double? _10h { get; set; }
+
+    [Column("10h30")]
+    public double? _10h30 { get; set; }
+
+    [Column("11h")]
+    public double? _11h { get; set; }
+
+    [Column("11h30")]
+    public double? _11h30 { get; set; }
+
+    [Column("12h")]
+    public double? _12h { get; set; }
+
+    [Column("12h30")]
+    public double? _12h30 { get; set; }
+
+    [Column("13h")]
+    public double? _13h { get; set; }
+
+    [Column("13h30")]
+    public double? _13h30 { get; set; }
+
+    [Column("14h")]
+    public double? _14h { get; set; }
+
+    [Column("14h30")]
+    public double? _14h30 { get; set; }
+
+    [Column("15h")]
+    public double? _15h { get; set; }
+
+    [Column("15h30")]
+    public double? _15h30 { get; set; }
+
+    [Column("16h")]
+    public double? _16h { get; set; }
+
+    [Column("16h30")]
+    public double? _16h30 { get; set; }
+
+    [Column("17h")]
+    public double? _17h { get; set; }
+
+    [Column("17h30")]
+    public double? _17h30 { get; set; }
+
+    [Column("18h")]
+    public double? _18h { get; set; }
+
+    [Column("18h30")]
+    public double? _18h30 { get; set; }
+
+    [Column("19h")]
+    public double? _19h { get; set; }
+
+    [Column("19h30")]
+    public double? _19h30 { get; set; }
+
+    [Column("20h")]
+    public double? _20h { get; set; }
+
+    [Column("20h30")]
+    public double? _20h30 { get; set; }
+
+    [Column("21h")]
+    public double? _21h { get; set; }
+
+    [Column("21h30")]
+    public double? _21h30 { get; set; }
+
+    [Column("22h")]
+    public double? _22h { get; set; }
+
+    [Column("22h30")]
+    public double? _22h30 { get; set; }
+
+    [Column("23h")]
+    public double? _23h { get; set; }
+
+    [Column("23h30")]
+    public double? _23h30 { get; set; }
+
+    [Column("24h")]
+    public double? _24h { get; set; }
+
+    public double? Tổng { get; set; }
+}
